3ef9ee67c2d3769df624143449268a51f60c56b4
Frédéric Glorieux <frederic.glorieux@fictif.org>
2021-08-10 18:28:51 +0200

    Code

--- build.php
+++ build.php
@@ -27,11 +27,11 @@ class HurlusBuild
   {
     $kindlegen = dirname(dirname(__FILE__))."/teinte/epub/kindlegen";
     foreach (array_merge(self::$publicfiles, self::$privatefiles) as $srcfile) {
-      $name = pathinfo($srcfile,  PATHINFO_FILENAME);
+      $name = pathinfo($srcfile, PATHINFO_FILENAME);
       if ($name[0] == '_' || $name[0] == '.') continue;
       preg_match('@^(.*?)(_|\-\d|\d)@', $name, $matches);
       $author = $matches[1];
-      $dstdir = dirname(__FILE__).'/'.$author.'/';
+      $dstdir = dirname(__FILE__).'/'.$name.'/';
       Tools::mkdir($dstdir);
       $dstpath = $dstdir.$name;
 
@@ -88,62 +88,94 @@ class HurlusBuild
   public static function readme()
   {
     include_once(dirname(dirname(__FILE__)).'/teinte/teidoc.php');
-    $readme = "
-# Auteurs / titres
+    $readme = '
+# Hurlus, un catalogue bénévole <a href="#" onmouseover="if(this.ok)return; this.href=\'mai\'+\'lt\'+\'o:lire\'+\'\\u0040\'+\'hurlus.fr\'; this.ok=true">🖂</a>
 
-";
+> Des bouquinistes électroniques, pour du texte libre à participations libres
+
+Chacun de ces textes a été aimé, ou haï, en tous cas a été lu, soigné, et parfois introduit d’une préface par une personne hurlue.
+Elle s’y est intéressé parce qu’elle a pensé que ces pages étaient nécessaires,
+nécessaires à sa réflexion du moment, à l’actualité, voire à l’intelligence de notre présent.
+Ce catalogue n’obéit à aucun parti, ne milite pas pour une cause, sauf celle de réflechir et de partager
+la matière de la réflexion.
+
+Ne vous étonnez donc pas si Paul de Tarse côtoie Marx ou Descartes. Les textes religieux, par exemple, sont fondateurs de civilisations, il ne suffit pas de se dire athée pour les réfuter, il vaut mieux s’en informer pour lire jusqu’où ils influencent la société, en bien et en mal.
+Les textes politiques, même ceux qui ne sont pas de notre
+bord, continuent de marquer l’histoire. L’action des philosophes est plus souterraine, ils expriment souvent l’esprit de leur culture. Il y a aussi de l’histoire, des fictions, des livres longs pour les écrans, et des textes plus courts à imprimer et faire circuler.
+
+L’édition électronique est soigneuse, tant sur la technique
+que sur l’établissement du texte ; mais sans aucune prétention scolaire, au contraire.
+Le but est de s’adresser à tous, sans distinction de science ou de diplôme, et d’attirer
+ceux qui souhaitent découvrir cette autre manière de lire : éditer.
+
+Chaque texte est diponible en plusieurs formats
+\\ <b title="Source XML/TEI" class="mime48 tei">[TEI]</b> [XML/TEI](https://www.tei-c.org/release/doc/tei-p5-doc/en/html/REF-ELEMENTS.html), source depuis laquelle tous les format qui suivent sont générés
+\\ <b title="EPUB, pour liseuses et téléphones" class="mime48 epub">[epub]</b> EPUB, livre électronique format ouvert (téléphones, liseuses…)
+\\ <b title="HTML une page" class="mime48 html">[html]</b> HTML, texte à lire en une page
+\\ <b title="Bureautique (LibreOffice, MS.Word)" class="mime48 docx">[docx]</b> DOCX, texte modifiable
+\\ <b title="Amazon.kindle" class="mime48 mobi">[kindle]</b> MOBI, livre électronique au format propriétaire Kindle
+\\ <b title="PDF à imprimer, A4 2 colonnes" class="mime48 pdf">[pdf]</b> PDF, A4 2 colonnes à imprimer
+\\ <b title="PDF à lire, A5 une colonne" class="mime48 pdf">[pdf]</b> PDF, A5 1 colonne à lire
+\\ <a title="PDF, brochure à agrafer, imposé pour imprimante recto/verso" class="mime48 brochure">[pdf]<b> PDF, brochure à agrafer, imposé pour imprimante recto/verso
+
+';
     $authorLast = '';
     $i = 1;
 
-    $fauth = null;
-    $authbib = '';
     foreach (self::$publicfiles as $srcfile) {
       $name = pathinfo($srcfile,  PATHINFO_FILENAME);
       if ($name[0] == '_' || $name[0] == '.') continue;
       preg_match('@^(.*?)(_|\-\d|\d)@', $name, $matches);
       $author = $matches[1];
-      $exportpath = dirname(__FILE__).'/'.$author.'/'.$name;
-      $dstdir = 'https://hurlus.github.io/'.$author.'/';
+      $exportpath = dirname(__FILE__).'/'.$name.'/'.$name;
+      $dstdir = 'https://hurlus.github.io/'.$name.'/';
       $dstpath = $dstdir.$name;
       $teidoc = new Teidoc($srcfile);
       $meta = $teidoc->meta();
+      // order titles by author in catalog
       if ($authorLast != $author) {
-        if ($author == 'bible') $meta['byline'] = 'Bible';
-        $fopen = dirname(__FILE__).'/'.$author.'/README.md';
-        $fauth = fopen($fopen, "w");
-        $readme .= "\n## ".'<a href="'.$author.'/">'.$meta['byline']."</a>\n\n";
-        fwrite($fauth, '# '.$meta['byline']."\n\n");
         $authorLast = $author;
+        if ($author == 'bible') $readme .= "\n## ". 'Bible'."\n\n";
+        else $readme .= "\n## ".$meta['byline']."\n\n";
       }
-      $authbib = '* ';
-      if ($meta['date']) $authbib .= $meta['date'].', ';
-      $authbib .= ' <a title="Source XML/TEI" class="file tei" href="https://hurlus.github.io/tei/'.basename($srcfile).'">[TEI]</a> ';
-      $authbib .= ' <a title="HTML une page" class="file html" href="'.$dstpath.'.html">[html]</a> ';
-      $authbib .= ' <a title="Bureautique (LibreOffice, MS.Word)" class="file docx" href="'.$dstpath.'.docx">[docx]</a> ';
-      $authbib .= ' <a title="Amazon.kindle" class="file mobi" href="'.$dstpath.'.mobi">[kindle]</a> ';
-      $authbib .= ' <a title="EPUB, pour liseuses et téléphones" class="file epub" href="'.$dstpath.'.epub">[epub]</a> ';
+      $bibl = '';
+      if ($meta['date']) $bibl .= $meta['date'].', ';
+      $bibl .= ' <a title="Source XML/TEI" class="mime tei" href="https://hurlus.github.io/tei/'.basename($srcfile).'">[TEI]</a> ';
+      $bibl .= ' <a title="HTML une page" class="mime html" href="'.$dstpath.'.html">[html]</a> ';
+      $bibl .= ' <a title="Bureautique (LibreOffice, MS.Word)" class="mime docx" href="'.$dstpath.'.docx">[docx]</a> ';
+      $bibl .= ' <a title="Amazon.kindle" class="mime mobi" href="'.$dstpath.'.mobi">[kindle]</a> ';
+      $bibl .= ' <a title="EPUB, pour liseuses et téléphones" class="mime epub" href="'.$dstpath.'.epub">[epub]</a> ';
       foreach (glob($exportpath."*.tex") as $exportfile) {
-        $authbib .= ' <a title="LaTeX" class="file tex" href="'.$dstdir.basename($exportfile).'">[TeX]</a> ';
+        $bibl .= ' <a title="LaTeX" class="mime tex" href="'.$dstdir.basename($exportfile).'">[TeX]</a> ';
       }
       foreach (glob($exportpath."*.pdf") as $exportfile) {
         if (strpos($exportfile, '_a5') !== false) {
-          $authbib .= ' <a title="PDF à lire, A5 une colonne" class="file a5" href="'.$dstdir.basename($exportfile).'">[pdf a5]</a> ';
+          $bibl .= ' <a title="PDF à lire, A5 une colonne" class="mime a5" href="'.$dstdir.basename($exportfile).'">[pdf a5]</a> ';
         }
         else if (strpos($exportfile, '_brochure') !== false) {
-          $authbib .= ' <a title="Brochure à agrafer, pdf imposé pour imprimante recto/verso" class="file brochure" href="'.$dstdir.basename($exportfile).'">[brochure]</a> ';
+          $bibl .= ' <a title="Brochure à agrafer, pdf imposé pour imprimante recto/verso" class="mime brochure" href="'.$dstdir.basename($exportfile).'">[brochure]</a> ';
         }
         else {
-          $authbib .= ' <a title="PDF à imprimer, A4 2 colonnes" class="file pdf" href="'.$dstdir.basename($exportfile).'">[pdf]</a> ';
+          $bibl .= ' <a title="PDF à imprimer, A4 2 colonnes" class="mime pdf" href="'.$dstdir.basename($exportfile).'">[pdf]</a> ';
         }
       }
 
 
-      $authbib .= ' <a href="'.$dstpath.'.html">' . $meta['title'].'</a>';
-      $authbib .= "\n";
+      $readme .= $bibl . ' <a href="'.$dstdir.'">' . $meta['title']."</a>\n";
+      // write a welcome page for the book
+      $fopen = fopen(dirname(__FILE__).'/'.$name.'/README.md', 'w');
+      fwrite($fopen, '# '.$meta['byline']."\n");
+      fwrite($fopen, '## '.$meta['title']."\n\n");
+      fwrite($fopen, '> '.str_replace('mime', 'mime48', $bibl)."\n");
+      fclose($fopen);
       $i++;
-      $readme .= $authbib;
-      fwrite($fauth, $authbib);
     }
+    $readme .= "
+Les hurlus furent aussi des rebelles protestants qui cassaient les statues dans les églises catholiques. En 1566 démarra la révolte des gueux dans le pays de Lille. L’insurrection enflamma la région jusqu’à Anvers où les gueux de mer bloquèrent les bateaux espagnols.
+Ce fut une rare guerre de libération dont naquit un pays toujours libre : les Pays-Bas.
+En plat pays francophone, par contre, restèrent des bandes de huguenots, les hurlus, progressivement réprimés par la très catholique Espagne.
+Cette mémoire d’une défaite est éteinte, rallumons-la. Sortons les livres du culte universitaire, cherchons les idoles de l’époque, pour les briser.
+";
     return $readme;
   }
 }
